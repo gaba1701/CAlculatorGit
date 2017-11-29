7355d8cd2f9932f166e4110b663dd7cf5a24beaf
Anna Barysheva <gannabarysheva@gmail.com>
2017-11-29 10:12:36 +0100

    first commit create test

---
+++ src/main/java/com/maven/CalculatorGit/CalculatorGit.java
@@ -0,0 +1,13 @@
+package com.maven.CalculatorGit;
+
+
+public class CalculatorGit {
+
+	
+	public double Add(double firstNumber, double secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	public double Substruc(double firstNumber, double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+}
---
+++ src/test/java/com/maven/CalculatorGit/CalculatorGitTest.java
@@ -0,0 +1,36 @@
+package com.maven.CalculatorGit;
+
+import static org.junit.Assert.*;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+public class CalculatorGitTest {
+	static final Logger LOG = Logger.getLogger(CalculatorGit.class.getName());
+	
+	CalculatorGit calculator = new CalculatorGit();
+	
+	Random random = new Random();
+	DecimalFormat df = new DecimalFormat();
+
+	@Test
+	public void testAdd() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<10;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*10));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*10));
+			result = firstNumber + secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.add(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
+}
